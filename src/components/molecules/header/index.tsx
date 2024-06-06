18b33a6476958f7f57a164d517bd17d3489dff13
reshmalb <reshma.lb27@gmail.com>
2024-06-06 14:00:33 +0530

    feat: Added Dashboard Layout

---
+++ src/components/molecules/header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full h-10 shadow-md  border border-gray-600 text-lg align-middle'>
+        Header
+      
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/template/dashboard-template/index.tsx
@@ -0,0 +1,32 @@
+
+import SideBar from "../../organisms/sidebar";
+import Header from "../../molecules/header";
+
+import React from "react";
+type Props = {
+  children: React.ReactNode;
+};
+
+const DashboardTemplate: React.FC<Props> = ({ children }) => {
+
+  return (
+    <div className="relative ">
+        <Header/>
+        <div className=" flex gap-10 ">
+            <div  className="mt-10">
+            <SideBar/>        
+
+            </div>
+            <div className="mt-10">
+            {children}  
+
+            </div>
+
+        </div>
+      
+   
+    </div>
+  );
+};
+
+export default DashboardTemplate;
