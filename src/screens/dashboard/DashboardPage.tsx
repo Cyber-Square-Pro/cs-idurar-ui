b088454b3be14c1722b3155a181930c10b71857a
adnaaaen <adnanmuhammedadnancp@gmail.com>
2024-06-13 21:34:28 +0530

    style: enhance dashboard layout

--- src/screens/dashboard/DashboardPage.tsx
+++ src/screens/dashboard/DashboardPage.tsx
@@ -1,11 +1,14 @@
-
-
+/*
+  Author: Mohammed Rifad on May 31st, 2024 
+  Purpose: Renders the main content of the dashboard page.
+  Updated by: - Muhammed Adnan on June 13th, 2024 - Enhance Style.
+*/
 const DashboardPage = () => {
   return (
-    <div className="  w-screen h-screen border border-gray-600">
-      DashboardPage
+    <div className="flex h-full w-full justify-center flex-col items-center overflow-y-auto bg-white p-5">
+    <h1 className="font-bold text-2xl text-slate-500" >DASHBOARD PAGE</h1> 
     </div>
-  )
-}
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
